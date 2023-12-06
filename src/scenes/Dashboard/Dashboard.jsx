28b58766d7e90d4d98477257ab6c8d7e03d9ac2e
AJ Johnson <aj132@icloud.com>
2023-12-06 12:21:43 -0500

    Fix styling and add header to Dashboard

--- src/scenes/Dashboard/Dashboard.jsx
+++ src/scenes/Dashboard/Dashboard.jsx
@@ -8,17 +8,11 @@ const Dashboard = ({ isCollapsed }) => {
   return (
     <div className={`dashboard-wrapper ${isCollapsed ? 'expanded' : 'collapsed'}`}>
       <div className="dashboard-content">
-        <div className="vertical-container">
-          <div className="horizontal-stack bg-red-100" >
-            <h1>Dashboard</h1>
-            <div>
-              <p>H</p>
-            </div>
-          </div>
-
+        <div className="dashboard-header">
+          <h1>Dashboard</h1>
         </div>
-
       </div>
+
     </div>
   );
 };
