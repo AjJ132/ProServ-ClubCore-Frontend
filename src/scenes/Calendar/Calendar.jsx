b3f1cf338085dc1212f6946f648574abe65833f1
AJ Johnson <aj132@icloud.com>
2023-12-16 11:48:02 -0500

    Add calendar container and quick info section

--- src/scenes/Calendar/Calendar.jsx
+++ src/scenes/Calendar/Calendar.jsx
@@ -13,7 +13,16 @@ const Calendar = () => {
             <h1>Calendar</h1>
             <div className="line-divider mt-4"></div>
             </div>
-            <RsuiteCalendar bordered cellClassName={date => (date.getDay() % 2 ? "calendar-bg-offset" : undefined)}/>
+            <div className="calendar-container">
+                <RsuiteCalendar bordered cellClassName={date => (date.getDay() % 2 ? "calendar-bg-offset" : undefined)}/>
+                <div className="calendar-quick-info section-border p-4">
+                    <h2>Coming Up</h2>
+                    <div className="line-divider"></div>
+                    <h3>Practice</h3>
+                    <h3>Meet</h3>
+                    <h3>Practice</h3>
+                </div>
+            </div>
         </div>
     );
 };
